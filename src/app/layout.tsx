42517c40657d7e1e691f71aa120d45e4ad88b52b
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-08-08 10:09:16 +0600

    project init and isntalled required packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { StyleProvider } from "@ant-design/cssinjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <StyleProvider hashPriority="high">
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </StyleProvider>
   );
 }
